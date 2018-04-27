7a0308f633629cf013dedccdeeca212ac19afa26
5931 <5931@ait-sydney.net.au>
2018-04-27 12:22:51 +1000

    first commit

---
+++ FormSubmitSignin.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+  <body>
+    <h1>
+      My First Heading
+    </h1>
+    <p>
+      My first paragraph
+    </p>
+      
+    <?php
+      
+      
+//      $servername = "localhost";
+//      $username = "root";
+//      $password = "";
+//      $dbname ="testing";
+//    
+//      $conn = new mysqli($servername, $username, $password, $dbname);
+//      if ($conn->connect_error) 
+//      {
+//        die("Connection failed: " . $conn->connect_error);
+//      } else{
+//        echo "Connected successfully from signin";
+//      }
+//    
+//    $sql = "INSERT INTO `testing`.`user` (`id`, `username`, `password`) VALUES ('0', 'cindy', 'ng');";
+//    //$sql = "UPDATE table1 SET firstname='cindy',lastname='ly' WHERE id=2";
+//    //$sql = "SELECT firstname, lastname FROM table1";
+//    //mysqli_query($conn, $sql)
+//    $result = $conn->query($sql);
+//  
+//
+//      $conn->close();
+      
+      $months = array("Jan"=>"35","Feb"=>"37","Mar"=>"43");
+      //asort($months);
+      print_r($months);
+    ?>
+  </body>
+</html>
